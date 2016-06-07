771679e694242c3eabe6f794f28bb4df225e9d87
Krzysztof Kosman <krzysztof@1000i.pl>
2016-06-07 10:28:00 +0200

    origin branch-0.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   scope '/tiny_mce_uploads' do
-    resources :attachments do 
-      collection do
-        get :list
-        get :delete
-        post :upload
-      end
-    end
+    resources :attachments
   end
 end
--- public/javascripts/tiny_mce/plugins/fupload/functions.js
+++
@@ -1,8 +0,0 @@
-
-function doInsert(src){
-  var html = '<img src="'+src+'" />';
-  tinyMCEPopup.editor.execCommand('mceInsertContent', false, html);
-  tinyMCEPopup.close();
-  return false;
-}
-
